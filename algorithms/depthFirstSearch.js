6f2d222ba200453827ae95d5302a34d85ae96534
Daniel <daniel.sola.fraire@gmail.com>
2019-10-11 20:06:54 +0200

    Added DFS algorithm

--- algorithms/depthFirstSearch.js
+++ algorithms/depthFirstSearch.js
@@ -1,50 +1,46 @@
-const vertex = [1,2,3,4,5,6,7];
-const edges = [[4,1], [1,3], [1,2], [3,6], [2,5], [5,6], [6,7]];
+const vertex = [1, 2, 3, 4, 5, 6, 7];
+const edges = [[4, 1], [1, 3], [1, 2], [3, 6], [2, 5], [5, 6], [6, 7]];
 const graph = [vertex, edges];
 
-const getNextVertex = (adjacentEdges, currentVertex, visitedVertices) => {
-    const adjacentVertices = [];
+const getAdjacentVertices = (vertex, edges) => {
+  const adjacentVertices = [];
 
-    for(const edges of adjacentEdges) {
-        const adjacentVertex = edges.find(
-          vertex =>
-            vertex !== currentVertex && !visitedVertices.includes(vertex)
-        );
+  for (const edge of edges) {
+    if (edge.includes(vertex)) {
+      const adjacentVertex = edge.filter(edgeVertex => edgeVertex !== vertex);
 
-        adjacentVertices.push(adjacentVertex);
+      adjacentVertices.push(...adjacentVertex);
     }
-    
-    const sortedAdjacentVertices = adjacentVertices.sort();
-    const nextVertex = sortedAdjacentVertices.shift();
-    
-    return nextVertex;
-}
+  }
 
-const exploreVertex = (currentVertex, visitedVertices, currentVertices, verticesStack) => {
-    currentVertices.push(currentVertex);
-    verticesStack.push(currentVertex);
-    
-    const adjacentEdges = edges.filter(edge => edge.includes(currentVertex));
-    const nextVertex = getNextVertex(adjacentEdges, currentVertex, visitedVertices);
-
-    visitedVertices.push(currentVertex);
-
-    return nextVertex;
+  return adjacentVertices.sort();
 };
 
 const dfs = graph => {
-    const [ vertex, edges ] = graph;
-    const startingVertex = vertex[0];
-    const verticesStack = [];
-    const visitedVertices = [];
-    const currentVertices = [];
+  const [vertex, edges] = graph;
+  const verticesStack = [];
+  const visitedVertices = {};
+  const startingVertex = vertex[0];
 
-    let currentVertex = startingVertex;
+  visitedVertices[startingVertex] = true;
+  verticesStack.push(startingVertex);
 
-    while(visitedVertices < vertex.length) {
-        let nextVertex = exploreVertex(currentVertex, visitedVertices, currentVertices, verticesStack);
-        currentVertex = nextVertex;
+  while (verticesStack.length) {
+    const currentVertex = verticesStack[verticesStack.length - 1];
+    console.log('Visiting vertex:', currentVertex);
+    const adjacentVertices = getAdjacentVertices(currentVertex, edges);
+    const unvisitedVertex = adjacentVertices
+      .filter(adjacentVertex => !visitedVertices[adjacentVertex])
+      .sort()
+      .shift();
+
+    if (unvisitedVertex) {
+      verticesStack.push(unvisitedVertex);
+      visitedVertices[unvisitedVertex] = true;
+    } else {
+      verticesStack.pop();
     }
-}
+  }
+};
 
 dfs(graph);
